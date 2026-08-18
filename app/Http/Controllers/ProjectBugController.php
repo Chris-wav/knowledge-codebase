3b7fce1136cf2d2e684ceefa97387db6a491c46a
chris-wav <chris-wav@users.noreply.github.com>
2026-08-18 16:39:29 +0300

    feat: prepare project bug filtering

--- app/Http/Controllers/ProjectBugController.php
+++ app/Http/Controllers/ProjectBugController.php
@@ -4,12 +4,13 @@ namespace App\Http\Controllers;
 
 use App\Http\Resources\BugResource;
 use App\Models\Project;
+use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 use Illuminate\Support\Facades\Gate;
 
 class ProjectBugController extends Controller
 {
-    public function index(Project $project): AnonymousResourceCollection
+    public function index(Project $project, Request $request): AnonymousResourceCollection
     {
         Gate::authorize('view', $project);
 
